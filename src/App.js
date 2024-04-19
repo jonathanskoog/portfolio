b6cfa375dea48d557cf9658abe05d44b28f1cd71
Jonathan Skoog <josk20@student.bth.se>
2024-04-19 22:53:29 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload dkljsoikskoi.
+          Edit <code>src/App.js</code> and save to reload dkljsoikscdscskoi.
         </p>
         <a
           className="App-link"
