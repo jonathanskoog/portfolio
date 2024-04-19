0ec385647b8c48d7a80826b0924644cf33049c6c
Jonathan Skoog <josk20@student.bth.se>
2024-04-19 22:39:48 +0200

    css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from "./logo.svg";
+import React from "react";
+import Test from "./About_me/Test";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reloadss.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Font">
+      <p className="text-center text-blue-400 text-7xl">HEj</p>
+      <Test />
     </div>
   );
 }
